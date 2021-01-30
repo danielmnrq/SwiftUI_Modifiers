3b0dd5c44ffa2576ffbd8e2625ed7b4ec1fa859e
Daniel Manrique Figueroa <daniel@manrique.tech>
2021-01-30 14:09:38 -0600

    Project Creation

---
+++ SwiftUI_Modifiers/SwiftUI_Modifiers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Modifiers
+//
+//  Created by Daniel Manrique Figueroa on 1/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Modifiers/SwiftUI_Modifiers/SwiftUI_ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ModifiersApp.swift
+//  SwiftUI_Modifiers
+//
+//  Created by Daniel Manrique Figueroa on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
